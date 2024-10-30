aa2e49b22aefccddb23504fe776b0ace90b15ce0
Amaan Ul Haq Siddiqui <amaanulhaq55@gmail.com>
2024-10-30 22:12:18 +0500

    while ++

---
+++ basics/while.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+    int i = 1;  
+
+    while (i <= 5) {
+        cout << "Number: " << i << endl;
+        i++;  
+    }
+
+    return 0;
+}
