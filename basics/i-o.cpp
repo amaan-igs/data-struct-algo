fcc62b93adf4b58f360913c032cf306f9e9c8609
Amaan Ul Haq Siddiqui <amaanulhaq55@gmail.com>
2024-10-30 20:04:29 +0500

    makerfile ++

---
+++ basics/i-o.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout << "getting fammiliar with <<>> operators" << endl;
+    cout << "Enter your name: ";
+    string name; 
+    cin >> name;
+    cout << "Hello, " << name << "!" << endl;
+    
+    return 0;
+}
