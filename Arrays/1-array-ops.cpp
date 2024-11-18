14fe1bc2feeb3e5ff10e88b757d108aacc900924
Amaan Ul Haq Siddiqui <amaanulhaq.s@outlook.com>
2024-11-18 18:37:46 +0500

    add sum, min, and max calculations for array elements in 1-array-ops.cpp

--- Arrays/1-array-ops.cpp
+++ Arrays/1-array-ops.cpp
@@ -16,4 +16,25 @@ int main() {
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
+
+    // sum and avg of array elements
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    cout << "\nSum of array elements: " << sum << std::endl;
+
+    // min and max of array elements
+    int min = arr[0], max = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    cout << "Min of array elements: " << min << std::endl;
+    cout << "Max of array elements: " << max << std::endl;
+
 }
